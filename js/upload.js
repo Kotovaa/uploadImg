822a4e1eacfb6097c3a78498d4be02f6af66de05
Aleksandr <sashjkeee@list.ru>
2016-07-22 13:07:29 +0300

    update plugin

--- js/upload.js
+++ js/upload.js
@@ -1,23 +1,16 @@
 $(document).ready(function() {
-    // Стандарный input для файлов
-    var fileInput = $('#file-field');
-    
-    // ul-список, содержащий миниатюрки выбранных файлов
+    var fileInput = $('#file-field');    
     var imgList = $('ul#img-list');
+     var dropBox = $('#img-container');
     function displayFiles(files) {
         var imageType = /image.*/;
         var num = 0;
         $('#img-list li').remove();
         $.each(files, function(i, file) {
-            // Отсеиваем не картинки
             if (!file.type.match(imageType)) {
                 return true;
             }
-            
             num++;
-            
-            // Создаем элемент li
-            // а также создаем ему свойство file, куда помещаем объект File (при загрузке понадобится)
             var li = $('<li/>').appendTo(imgList);
             var img = $('<img/>').appendTo(li);
             li.get(0).file = file;
@@ -30,17 +23,11 @@ $(document).ready(function() {
             reader.readAsDataURL(file);
         });
     }
-    
-    // Обработка события выбора файлов через стандартный input
-    // (при вызове обработчика в свойстве files элемента input содержится объект FileList,
-    //  содержащий выбранные файлы)
     fileInput.bind({
         change: function() {
             displayFiles(this.files);
         }
     });
-  
-    // Проверка поддержки File API в браузере
     if(window.FileReader == null) {
         alert('Ваш браузер не поддерживает File API!');
     }
